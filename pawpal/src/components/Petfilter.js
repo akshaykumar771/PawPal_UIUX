5171454d2c44d64fa02093ae50dd3ee1542c8153
Gautam Rattihalli <gautamr2590@gmail.com>
2019-12-09 11:12:29 +0100

    Modifications

--- pawpal/src/components/Petfilter.js
+++ pawpal/src/components/Petfilter.js
@@ -2,14 +2,37 @@ import React from "react";
 
 export default function Petfilter({ petfilter }) {
     return (
-        <div className="col-md-12">
-            <div className="col-md-3">
+        <div className="row col-md-12 filter-class">
+            <div className="col-md-4">
                 <div class="form-group">
                     <label for="pettype">Cat or Dog ?</label>
                     <select class="form-control">
-                    <option>Cat</option>
-                    <option>Dog</option>
+                        <option>Cat</option>
+                        <option>Dog</option>
                     </select>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div class="form-group">
+                    <label for="pettype">Gender</label>
+                    <select class="form-control">
+                        <option>Male</option>
+                        <option>Female</option>
+                    </select>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="form-group">
+                <label htmlFor="price">Age of the Pet</label>
+                {/* <input
+                    type="range"
+                    name="price"
+                    min={minPrice}
+                    max={maxPrice}
+                    id="price"
+                    value={price}
+                    onChange={handleChange}
+                    className="form-control"/> */}
                 </div>
             </div>
         </div>
