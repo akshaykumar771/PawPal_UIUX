38fc2792ee8f52cc3ed3449473ec73f538232c6a
akshaykumar771 <akshaykumar771@gmail.com>
2019-12-16 15:26:51 +0100

    sidenav

--- pawpal/src/components/SideNav.js
+++ pawpal/src/components/SideNav.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { Component } from 'react';
 import { FaPaw } from "react-icons/fa"
 import Popup from "reactjs-popup";
 import MyForm from "./Form";
 import "./styles.css";
+import {Link} from "react-router-dom";
+import { PetContext } from "../context";
+import Cats from "../pages/Cats";
+import Pets from "../pages/Pets";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
-export default function SideNav({ sidenav }) {
+export default class SideNav extends Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+      this.state = {
+        flag : false
+      }
+    
+  }
+  handleClick = (e) =>{
+   this.setState = {
+     flag: !this.state.flag
+   } 
+ }
+  render() {
     return (
       <div className="col-md-3 sidenav">
         <h1> <FaPaw /> PawPal</h1>
- 
-        <a href="/pets">Services</a>
+       <div class="text-element content-element circles-list">
+<ol>
+<li><Link to ="/choosepets/">Dogs or Cats??</Link>
+</li>
+<li>
+  {this.state.flag?<Link to="/choosepets/Dogs">Pet Basics </Link>: <Link to="/choosepets/Cats">Pet Basics </Link>}
+
+  </li>
+<li><Link to ="/">Pet Details</Link></li>
+</ol>
+</div>
+
+        
         {/* <a href="#">Contact</a> */}
         <Popup modal trigger={<a href="#">Contact</a>}>
               <MyForm />
@@ -18,3 +48,4 @@ export default function SideNav({ sidenav }) {
 
     );
   }
+}
